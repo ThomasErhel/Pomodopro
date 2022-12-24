224137a6c262155d34ce8ccf5118fc9cfa6d5084
Thomas Erhel <thomas.erhel@gmail.com>
2022-12-24 07:37:36 +0100

    Add README.md

--- Pomodopro Watch App/ContentView.swift
+++ Pomodopro Watch App/ContentView.swift
@@ -2,20 +2,68 @@
 //  ContentView.swift
 //  Pomodopro Watch App
 //
-//  Created by Thomas Erhel on 24/12/2022.
+//  Created by Thomas Erhel on 23/12/2022.
 //
 
 import SwiftUI
+import Foundation
+import WatchKit
+import UserNotifications
 
 struct ContentView: View {
+    @State private var timeRemaining: TimeInterval = 0.10 * 60
+    @State private var isWorkTime = true
+    @State private var timer: Timer?
+    @State private var extendedRuntimeSession: WKExtendedRuntimeSession?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(isWorkTime ? "Work 👨‍💻" : "Relax 🚶‍♂️")")
+                .font(.title)
+                .padding(.vertical)
+            
+            Text("\(String(format: "%02d:%02d", Int(timeRemaining) / 60, Int(timeRemaining) % 60))")
+                .font(.title)
+                .padding(.vertical)
         }
-        .padding()
+        .onAppear {
+            self.startTimer()
+        }
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.timeRemaining -= 1
+            
+            if self.timeRemaining == 0 {
+                self.timer?.invalidate()
+                WKInterfaceDevice.current().play(.success)
+                
+                if self.isWorkTime {
+                    self.showNotification(title: "Gratz! 🙌👏", body: "Let's chill 😁")
+                    self.timeRemaining = 5 * 60
+                    self.isWorkTime = false
+                } else {
+                    self.showNotification(title: "Back to work", body: "🤓")
+                    self.timeRemaining = 0.10 * 60
+                    self.isWorkTime = true
+                }
+                
+                self.startTimer()
+            }
+        }
+        
+        extendedRuntimeSession = WKExtendedRuntimeSession()
+        extendedRuntimeSession?.start()
+    }
+    
+    private func showNotification(title: String, body: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        
+        let request = UNNotificationRequest(identifier: "pomodoro_timer", content: content, trigger: nil)
+        UNUserNotificationCenter.current().add(request)
     }
 }
 
@@ -24,3 +72,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
---
+++ README.md
@@ -0,0 +1,18 @@
+# Pomodopro
+#### Video Demo: https://youtu.be/kzsLSwP-UuI
+#### Description:
+
+##### Pomodoro Timer for Apple Watch
+This is a Pomodoro timer application for the Apple Watch. The Pomodoro technique is a time management method that helps users break down their work into 25-minute intervals, separated by short breaks.
+
+The Pomodoro timer for Apple Watch has the following features:
+
+- A 25-minute timer that starts automatically when the application is launched.
+- A notification that is displayed when the 25-minute timer ends, indicating that it's time for a break.
+- A 5-minute timer that starts automatically after the break notification is displayed.
+- A notification that is displayed when the 5-minute timer ends, indicating that it's time to get back to work.
+- The ability to continue the timer even when the application is not open or when the device's screen is turned off, using the WKExtendedRuntimeSession class.
+
+To use the Pomodoro timer for Apple Watch, simply launch the application and start working. The timer will countdown and a notification will be displayed when it's time for a break. After the break, another notification will be displayed to indicate that it's time to get back to work. The timer will continue running in the background even when the application is not open or when the device's screen is turned off.
+
+I hope you find this Pomodoro timer for Apple Watch useful for managing your time and staying focused on your work!
