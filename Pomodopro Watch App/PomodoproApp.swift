a8fc82a376b85e68a88c909dcb237d5e35f88a93
Thomas Erhel <thomas.erhel@gmail.com>
2022-12-24 04:23:26 +0100

    Initial Commit

---
+++ Pomodopro Watch App/PomodoproApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoproApp.swift
+//  Pomodopro Watch App
+//
+//  Created by Thomas Erhel on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Pomodopro_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
